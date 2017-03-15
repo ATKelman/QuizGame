a2ed8dc55464d7ddeb16a3d257d56282b41582f6
ATKelman <a14aleke@student.his.se>
2017-03-15 13:10:36 +0100

    Start

---
+++ Networking Test - Quiz Game/Assets/Script/Client/Client.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class Client : MonoBehaviour
+{
+    public GameObject chatContainer;
+    public GameObject messagePrefab;
+
+    private bool socketReady = false;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectToServer()
+    {
+        // if already conneted, ignore
+        if (socketReady)
+            return;
+
+        // default host / port
+        string host = "127.0.0.1";
+        int port = 6321;
+
+        // Overwrite default host / port values, if there is something in those boxes
+        string h;
+        int p;
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+            host = h;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+            port = p;
+
+        // Create the socket
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error : " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if(socketReady)
+        {
+            if(stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data); 
+            }
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        GameObject instance = Instantiate(messagePrefab, chatContainer.transform) as GameObject;
+        instance.GetComponentInChildren<Text>().text = data;
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+
+}
---
+++ Networking Test - Quiz Game/Assets/Script/Server/Server.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System;
+using System.Net;
+using System.IO;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectList;
+
+    public int port = 6321;
+
+    private TcpListener server;
+    private bool serverStarted;
+
+    private void Start()
+    {
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListening();
+            serverStarted = true;
+
+            Debug.Log("server has been started on port " + port.ToString());
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        if (!serverStarted)
+            return;
+
+        foreach(ServerClient c in clients)
+        {
+            // Is the client still connected?
+            if(!IsConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectList.Add(c);
+                continue;
+            }
+            // Check for messages from client
+            else
+            {
+                NetworkStream s = c.tcp.GetStream();
+                if(s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if(data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient( AcceptTcpClient, server);
+    }
+    private bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+
+                return true;
+            }
+            else
+                return false;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+
+        StartListening();
+
+        // send a message to everyone, telling of new connection
+        Broadcast(clients[clients.Count - 1].clientName + " has connected", clients);
+    }
+
+    private void OnIncomingData(ServerClient c, string data)
+    {
+        Debug.Log(c.clientName + " has sent message " + data);
+        Broadcast(data, clients);
+    }
+    private void Broadcast(string data, List<ServerClient> cl)
+    {
+        foreach(ServerClient c in cl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+            }
+            catch(Exception e)
+            {
+                Debug.Log("Write error : " + e + " to client " + c.clientName);
+            }
+        }
+    }
+}
+
+public class ServerClient
+{
+    public TcpClient tcp;   // using system.net.sockets
+    public string clientName;
+
+    public ServerClient(TcpClient clientSocket)
+    {
+        clientName = "Guest";
+        tcp = clientSocket;
+    }
+}
